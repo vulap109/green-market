2d4904030ead06a33123104e9dbb18ae333e46bd
Vu Lap <vuquanglap109@gmail.com>
2026-03-19 23:08:31 +0700

    update img cover

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let isMenuOpen = false;
 function toggleContact() {
     isMenuOpen = !isMenuOpen;
 
+    const container = document.getElementById('contact-container');
     const menu = document.getElementById('contact-menu');
     const chatIcon = document.getElementById('btn-chat-icon');
     const closeIcon = document.getElementById('btn-close-icon');
@@ -11,6 +12,8 @@ function toggleContact() {
         // open menu
         menu.classList.remove('opacity-0', 'scale-95', 'pointer-events-none', 'translate-y-4');
         menu.classList.add('opacity-100', 'scale-100', 'pointer-events-auto', 'translate-y-0');
+
+        container.classList.add('z-50');
 
         // Rotate and hide icon chat
         chatIcon.classList.remove('rotate-0', 'opacity-100');
@@ -23,6 +26,8 @@ function toggleContact() {
         // close menu
         menu.classList.add('opacity-0', 'scale-95', 'pointer-events-none', 'translate-y-4');
         menu.classList.remove('opacity-100', 'scale-100', 'pointer-events-auto', 'translate-y-0');
+
+        container.classList.remove('z-50');
 
         // return icon chat
         chatIcon.classList.add('rotate-0', 'opacity-100');
